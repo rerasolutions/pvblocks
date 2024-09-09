2665edee721c5ccb2bded4bd27e0bff4d6a4c60a
Erik Haverkamp <erik.haverkamp@rerasolutions.com>
2024-09-09 13:24:57 +0200

    added sweepstyle file??

---
+++ pvblocks-api/pvblocks-api/SweepStyle.cs
@@ -0,0 +1,12 @@
+﻿namespace pvblocks_api;
+
+public enum SweepStyle
+{
+    ISC_TO_VOC = 0,
+    SWEEP_VOC_TO_ISC = 1,
+    EXTENT_CURVE_DELAY = 2,
+    SWEEP_VOC_ISC_VOC = 4,
+    SWEEP_ISC_VOC_ISC = 8,
+    SWEEP_FAST_PMAX = 16
+
+}
